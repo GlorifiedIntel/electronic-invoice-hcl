49ef8b79383405fb4ef22d6561cb901a76888463
KEVIN CROSS MINCHAKPU <165849310+GlorifiedIntel@users.noreply.github.com>
2025-01-20 06:48:18 +0100

    commit

--- .history/src/app/page_20250120062509.tsx
+++ .history/src/app/page_20250120062509.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     
-      <main className="flex flex-col justify-center h-full text-center max-w-5xl mx-auto my-10">
+      <main className="max-w-5xl mx-auto">
         <h1 className="text-3xl font-bold gap-6 digit">Electronic</h1>
         <h3 className="text-5xl font-bold">Sales Invoice</h3>
         <p className="mb-5 digit-2">App Version: 1.0</p>
